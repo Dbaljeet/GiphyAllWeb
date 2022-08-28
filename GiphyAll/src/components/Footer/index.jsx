0153d758a9e63c9d8f2cb4b4d31fd4213ef1d150
Angelo Berrios <angelo.berriosgrafit@gmail.com>
2022-08-28 03:50:49 -0400

    [Add] svg Linkedin, Github

--- GiphyAll/src/components/Footer/index.jsx
+++ GiphyAll/src/components/Footer/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import Github from "../../assets/Github";
+import Linkedin from "../../assets/Linkedin";
 const Footer = () =>{
     return(
         <>
@@ -7,28 +8,22 @@ const Footer = () =>{
 
                 <p>web realizada con motivos de estudio personal</p>
 
+                <a href="https://www.linkedin.com/in/angelo-berrios-pinto-96b317235/" target="_blank" rel="noopener noreferrer">
+                    LinkedIn
+                    <Linkedin width='25px'></Linkedin>
+                </a>
+
                 <a href="https://github.com/Dbaljeet" target="_blank" rel="noopener noreferrer">
-                    Personal Account
-                    <img src="GitHub.png" alt="github">
-                    </img>
+                    Cuenta personal
+                    <Github width='30px'></Github>
                 </a>
 
                 <a href="https://github.com/Angelobpixel" target="_blank" rel="noopener noreferrer">
-                    Student Account
-                    <img src="GitHub.png" alt="github">
-                    </img>
-                </a>
-
-                <a href="https://twitter.com/AngeloskyOner" target="_blank" rel="noopener noreferrer">
-                    
-                    <img src="twitter.png" alt="twitter"></img>
-                    Twitter
+                    Cuenta estudiante
+                    <Github width='30px'></Github>
                 </a>
                 
-                <a href="https://www.linkedin.com/in/angelo-berrios-pinto-96b317235/" target="_blank" rel="noopener noreferrer">
-                    LinkedIn
-                    <img src="Linkedin.png" alt="linkedin"></img>
-                </a>
+                
 
             </div>
         </>
